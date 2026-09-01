a27eea4c0d43d103f5df4adc0578c6c3316d6359
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-09-01 13:01:27 -0500

    cuadrado listo

---
+++ figuras/Cuadrado.java
@@ -0,0 +1,36 @@
+package com.krakedev.figuras;
+
+public class Cuadrado extends Figura {
+
+    private int lado;
+
+    public Cuadrado(String nombre, String color, int lado) {
+        super(nombre, color);
+        this.lado = lado;
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public int calcularPerimetro() {
+        return 4 * lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadrado [nombre=" + getNombre() + 
+               ", color=" + getColor() + 
+               ", lado=" + lado + "]";
+    }
+}
